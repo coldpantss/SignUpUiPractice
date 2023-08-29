5d1ce6c5a9b9e83f16aca0acf2a0c1f766b77533
youmean <youmin1121@gamail.com>
2023-08-29 10:43:53 +0900

    Initial commit

---
+++ app/src/main/java/com/youmean/signupuipractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.youmean.signupuipractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
